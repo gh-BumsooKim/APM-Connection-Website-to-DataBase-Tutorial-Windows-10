6620e71d2ce82e930a727f5e3c10a09caf086179
Bumsoo Kim <cmng828rhuypqq@cau.ac.kr>
2020-10-22 01:06:54 +0900

    Add files via upload

---
+++ MySQL-PRACTICE/Prac2_DML-INSERT.sql
@@ -0,0 +1,27 @@
+insert into userTBL values ('YJS', '유재석', 1972, '서울', '010', 11111111, 178, '08/8/8');
+insert into userTBL values ('KHD', '강호동', 1970, '경북', '011', '22222222', 182, '07/7/7');
+insert into userTBL values ('KKJ', '김국진', 1965, '서울', '019', '33333333', 171, '09/9/9');
+insert into userTBL values ('KYM', '김용만', 1967, '서울', '010', '44444444', 177, '15/5/5');
+insert into userTBL values ('KJD', '김제동', 1974, '경남', null, null, 173, '13/3/3');
+insert into userTBL values ('NHS', '남희석', 1971, '충남', '016', '66666666', 180, '17/4/4');
+insert into userTBL values ('SDY', '신동엽', 1971, '경기', null, null, 176, '08/10/10');
+insert into userTBL values ('LHJ', '이휘재', 1972, '경기', '011', '88888888', 180, '06/4/4');
+insert into userTBL values ('LKK', '이경규', 1960, '경남', '018', '99999999', 170, '04/12/12');
+insert into userTBL values ('PSH', '박수홍', 1970, '서울', '010', '00000000', 183, '12/5/5');
+
+select * from userTBL;
+
+insert into buyTBL values (1, 'KHD', '운동화', null, 30, 2);
+insert into buyTBL values (2, 'KHD', '노트북', '전자', 1000, 1);
+insert into buyTBL values (3, 'KYM', '모니터', '전자', 200, 1);
+insert into buyTBL values (4, 'PSH', '모니터', '전자', 200, 5);
+insert into buyTBL values (5, 'KHD', '청바지', '의류', 50, 3);
+insert into buyTBL values (6, 'PSH', '메모리', '전자', 80, 10);
+insert into buyTBL values (7, 'KJD', '책', '서적', 15, 5);
+insert into buyTBL values (8, 'LHJ', '책', '서적', 15, 2);
+insert into buyTBL values (9, 'LHJ', '청바지', '의류', 50, 1);
+insert into buyTBL values (10, 'PSH', '운동화', null, 30, 2);
+insert into buyTBL values (11, 'LHJ', '책', '서적', 15, 1);
+insert into buyTBL values (12, 'PSH', '운동화', null, 30, 2);
+
+select * from buyTBL;

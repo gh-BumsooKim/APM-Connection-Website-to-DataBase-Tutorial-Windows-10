40f63d156cb0e1b62434ec94062ef2f000cdc1f0
Bumsoo Kim <cmng828rhuypqq@cau.ac.kr>
2020-11-17 00:23:06 +0900

    html에서 받은 데이터를 출력하는 코드 및 DB 접속 확인 코드 추가
    POST 방식으로 html에서 데이터를 받고 브라우저에 출력 코드, DB 접속 확인 여부 코드 추가

--- php/svs.php
+++ php/svs.php
@@ -1,7 +1,19 @@
 <?php
-  /* php-html 연동 */
+	/* php-html 연동 */
 	$userID = $_POST["userID"];
 	$userPASSWORD = $_POST["userPASSWORD"];
+	// GET 방식의 보안성 문제로 POST 방식 사용
+	
+	echo "전달 받은 아이디 :" , $userID, "<br>";
+	echo "전달 받은 비밀번호 :" , $userPASSWORD, "<br>"; 
 
-  /* php-mysql 연동 */
+	/* php-mysql 연동 */
+	$con=mysqli_connect("localhost","TestUSER","1234") or die("MySQL 접속 실패");
+	if(mysqli_connect_error($con)){
+		echo "mysql 접속 실패" , "<br>";
+		echo "오류 : " , mysqli_connect_error();
+		exit();
+	}
+	echo "mysql 접속 성공";
+    mysqli_close($con);
 ?>

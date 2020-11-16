dc5a263f499f6235a3ce95a0a3ddb0240d84a8da
Bumsoo Kim <cmng828rhuypqq@cau.ac.kr>
2020-11-17 00:19:57 +0900

    파일 이동 및 브라우저 실행 코드 추가
    .html, .php 파일 자동 이동 및 크롬 브라우저로 localhost 실행 쉘 코드 추가

--- sh/exe.sh
+++ sh/exe.sh
@@ -1,5 +1,18 @@
-// file move
-mv
+# 'cp' command = copy and paste file
+# 'cd' command = change directory
 
-// change directory
-cd
+cd ../website/
+cp index.html C:/xampp/htdocs/index.html
+# mv index.html C:/xampp/htdocs/index.html
+
+cd ../php/
+cp svs.php C:/xampp/htdocs/svs.php
+# mv svs.php C:/xampp/htdocs/svs.php
+
+# Open Chrome Browser
+start chrome http://localhost/index.html
+
+:<<'END'
+Instead of 'cp' command, yon can 'mv' command 
+Not to copy and paste But to move files completely.
+END

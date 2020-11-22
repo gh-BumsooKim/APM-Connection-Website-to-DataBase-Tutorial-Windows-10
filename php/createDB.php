69f7bdbfdd01e94af52a884f2a0a9f689f7250e7
Bumsoo Kim <cmng828rhuypqq@cau.ac.kr>
2020-11-23 00:26:40 +0900

    Add Data Transmission Code
    code for Cookie on Client.

--- php/createDB.php
+++ php/createDB.php
@@ -1,22 +1,29 @@
 <?php
 	/* php-mysql 연동 */
 	$con=mysqli_connect("localhost","TestUSER","1234") or die("MySQL 접속 실패");
-	if(mysqli_connect_error($con)){
-		echo "mysql 접속 실패" , "<br>";
-		echo "오류 : " , mysqli_connect_error() , "<br>";
-		exit();
-	}
 	
-	$DBname = $_POST["DBname"];
+	$DBname = $_POST["DBname"];	
 	
 	/* DataBase 생성 */
 	$sql = "CREATE DATABASE"." ".$DBname;
 	$ret = mysqli_query($con,$sql);
 	if($ret){
-		echo "TestDB 생성" , "<br>";
+		echo $DBname." "."DB 생성 성공","<br>";
+		
+		/* Using Session or Cookie for data transmission 
+		 
+			1. Session : store data in Server
+				session_start();
+				$_SESSION['NAME'] = $DBname;
+		 
+			2. Cookie : store data in Client 
+				setcookie('NAME',variable);
+		*/
+		setcookie('DBname',$DBname);
+		echo "<A HREF='createTB.php'>--테이블 만들기--</A>";	
 	}
 	else{
-		echo "오류 : " , mysqli_error($con) , "<br>";
+		echo $DBname."DB 생성 오류 : " , mysqli_error($con) , "<br>";
 	}
 	
     mysqli_close($con);

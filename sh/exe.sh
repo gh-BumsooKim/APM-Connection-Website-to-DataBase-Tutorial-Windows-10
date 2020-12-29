86a75f163258c58753d024231d527be6a265d226
Bumsoo Kim <cmng828rhuypqq@cau.ac.kr>
2020-12-29 11:45:57 +0900

    Add echo

--- sh/exe.sh
+++ sh/exe.sh
@@ -1,17 +1,23 @@
 # 'cp' command = copy and paste file
 # 'cd' command = change directory
 
+echo "Copy createDB.html file to C:/xampp/htdocs ..."
+
 cd ../website/
 cp createDB.html C:/xampp/htdocs/createDB.html
 # mv createDB.html C:/xampp/htdocs/createDB.html
+
+echo "Copy all php file to C:/xampp/htdocs ..."
 
 cd ../php/
 find . -name '*.php' -exec cp {} C:/xampp/htdocs \;
 # find . -name '*.php' -exec mv {} C:/xampp/htdocs \;
 
+echo "Start chrome web browser to localhost"
+
 # Open Chrome Browser
 start chrome http://localhost/createDB.html
-#start chrome http://xxx.xxx.xxx.xxx/creteaDB.html
+#start chrome http://xxx.xxx.xxx.xxx/createDB.html
 # **input your IPv4 Address** ^^^ here 
 
 :<<'END'
